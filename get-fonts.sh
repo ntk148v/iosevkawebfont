0c52bca22104e380b342e0d368e56478487d1008
Kien Nguyen <kiennt2609@gmail.com>
2022-01-24 09:59:14 +0700

    Update script

--- get-fonts.sh
+++ get-fonts.sh
@@ -5,12 +5,13 @@ latest_release=$(curl --silent "https://api.github.com/repos/$repo/releases/late
 latest_release_num_only=${latest_release/v/""}
 # Check directory exists
 current_release=$(cat LATEST_RELEASE)
-if [[ ${current_release} -eq ${latest_release_num_only} ]]; then
+if [[ "${current_release}" == "${latest_release_num_only}" ]]; then
     echo "Repository is up-to-date"
     exit 0
 fi
 # Get release
 curl -L https://github.com/be5invis/Iosevka/releases/download/${latest_release}/webfont-iosevka-${latest_release_num_only}.zip > ${latest_release_num_only}.zip
+rm -rf latest/
 unzip ${latest_release_num_only}.zip -d latest
 # Clean up
 rm ${latest_release_num_only}.zip
